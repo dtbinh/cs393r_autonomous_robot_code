44fe16d2b24c3649d7024fc89c8fca16511aa22b
Joshua James <josh.ar.james@gmail.com>
2015-07-23 13:12:43 -0500

    Added rpp utils

---
+++ rpp_utils/include/rpp_utils/text_color_definitions.hpp
@@ -0,0 +1,12 @@
+#ifndef TEXT_COLOR_DEFINITIONS_HPP
+#define TEXT_COLOR_DEFINITIONS_HPP
+
+namespace rpp
+{
+  #define WHITE_TEXT  "\033[0m"
+  #define RED_TEXT "\033[31m"
+  #define YELLOW_TEXT  "\033[33m"
+  #define GREEN_TEXT  "\033[92m"
+}
+
+#endif //TEXT_COLOR_DEFINITIONS_HPP

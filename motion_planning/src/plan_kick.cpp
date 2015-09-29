244f4371532edd999005aad6bf23e306f10a081c
Joshua James <josh.ar.james@gmail.com>
2015-09-29 14:35:00 -0500

    More kick tweaks

--- motion_planning/src/plan_kick.cpp
+++ motion_planning/src/plan_kick.cpp
@@ -6,7 +6,7 @@ namespace motion_planning
       m_nh(nh)
   {
     m_nh.param("loop_rate", m_frame_rate, 30.0);
-    m_nh.param("lift_height", m_lift_height, 0.035);
+    m_nh.param("lift_height", m_lift_height, 0.04);
     m_nh.param("kick_time", m_kick_time, 0.5);
     m_nh.param("kick_dist", m_kick_dist, 0.1);
     m_nh.param("kick_filename", m_kick_filename, std::string("kick_trajectory.py"));
@@ -55,7 +55,7 @@ namespace motion_planning
     std::cerr << "Planning shift" << std::endl;
     planMove(0.0, -foot_separation, 0.0, 0.025, 0.005, 6.0);
     std::cerr << "Planning lift" << std::endl;
-    planMove(0.0, -foot_separation, m_lift_height, 0.025, 0.01, 6.0);
+    planMove(0.0, -foot_separation, m_lift_height, 0.025, 0.0075, 6.0);
     // std::cerr << "Planning kick" << std::endl;
     // planMove(m_kick_dist, -foot_separation, m_lift_height, 0.025, 0.0, m_kick_time);
 

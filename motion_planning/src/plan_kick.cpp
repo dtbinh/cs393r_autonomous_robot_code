8d780c25b2039edfef82a818e3cb2c040c4971da
Joshua James <josh.ar.james@gmail.com>
2015-09-29 16:49:30 -0500

    More kick tweaks

--- motion_planning/src/plan_kick.cpp
+++ motion_planning/src/plan_kick.cpp
@@ -6,9 +6,9 @@ namespace motion_planning
       m_nh(nh)
   {
     m_nh.param("loop_rate", m_frame_rate, 30.0);
-    m_nh.param("lift_height", m_lift_height, 0.01);
+    m_nh.param("lift_height", m_lift_height, 0.03);
     m_nh.param("kick_time", m_kick_time, 0.5);
-    m_nh.param("kick_dist", m_kick_dist, 0.1);
+    m_nh.param("kick_dist", m_kick_dist, 0.08);
     m_nh.param("kick_filename", m_kick_filename, std::string("kick_trajectory.py"));
                                       
     std::string joint_string =       "HeadYaw, HeadPitch, LHipYawPitch, LHipRoll, LHipPitch, LKneePitch, LAnklePitch, LAnkleRoll, RHipYawPitch, RHipRoll, RHipPitch, RKneePitch, RAnklePitch, RAnkleRoll, LShoulderPitch, LShoulderRoll, LElbowYaw, LElbowRoll, RShoulderPitch, RShoulderRoll, RElbowYaw, RElbowRoll, LWristYaw, LHand, RWristYaw, RHand";
@@ -53,16 +53,18 @@ namespace motion_planning
     m_joint_plan.push_back(m_js.position);
     double foot_separation = 0.1;
     std::cerr << "Planning shift" << std::endl;
-    planMove(0.0, -foot_separation, 0.0, 0.0, 0.03, 0.0, 2.0);
-    std::cerr << "Planning lift" << std::endl;
-    planMove(0.0, -foot_separation, m_lift_height, 0.1, 0.03, 0.0, 2.0);
+    planMove(0.0, -foot_separation, 0.0, 0.0, 0.03, 0.005, 1.0);
+    // std::cerr << "Planning lift" << std::endl;
+    // planMove(0.0, -foot_separation, m_lift_height, 0.1, -0.1, 0.0, 2.0);
     // std::cerr << "Planning lift 2" << std::endl;
     // planMove(0.0, -foot_separation, m_lift_height, 0.0, 0.025, 0.0, 2.0);
     std::cerr << "Planning kick" << std::endl;
-    planMove(m_kick_dist, -foot_separation, m_lift_height, -0.3, 0.03, 0, 0.5);
+    planMove(m_kick_dist, -foot_separation, m_lift_height, 0.0, 0.03, 0, 0.25);
 
     std::cerr << "Planning lunge" << std::endl;
-    planMove(m_kick_dist, -foot_separation, 0.0, 0.0, 0.03, 0, 0.5);
+    planMove(m_kick_dist, -foot_separation, 0.0, 0.0, 0.03, 0, 0.25);
+    std::cerr << "Planning delay" << std::endl;
+    planMove(m_kick_dist, -foot_separation, 0.0, 0.0, 0.03, 0, 3.0);
 
     // std::cerr << "Planning return" << std::endl;
     // planMove(0.0, -foot_separation, m_lift_height, 0.025, 0.0, 0.5);

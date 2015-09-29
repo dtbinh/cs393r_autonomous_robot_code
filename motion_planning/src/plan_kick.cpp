d093d9966f47afbc0dc2a2c2fef9618fae38e5f9
Joshua James <josh.ar.james@gmail.com>
2015-09-29 14:05:39 -0500

    More kick tweaks

--- motion_planning/src/plan_kick.cpp
+++ motion_planning/src/plan_kick.cpp
@@ -55,7 +55,7 @@ namespace motion_planning
     std::cerr << "Planning shift" << std::endl;
     planMove(0.0, -foot_separation, 0.0, 0.025, 0.0, 6.0);
     std::cerr << "Planning lift" << std::endl;
-    planMove(0.0, -foot_separation, m_lift_height, 0.025, 0.0, 6.0);
+    planMove(0.0, -foot_separation, m_lift_height, 0.025, 0.005, 6.0);
     // std::cerr << "Planning kick" << std::endl;
     // planMove(m_kick_dist, -foot_separation, m_lift_height, 0.025, 0.0, m_kick_time);
 
